64ff96f9e4914be0d071d440f5071f70075de0fa
LucioStedile <https://github.com/LucioStedile>
2022-04-12 01:35:21 -0300

    Páginas de texte do angular com diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'aula-diretivas';
+export class AppComponent implements OnInit{
+  count = 0;
+  nome = 'Lúcio Gentil Freitas Stedile';
+  text = '';
+
+  pessoas= [
+    {
+      nome: "Lúcio",
+      sobrenome: "Stedile"
+    },
+    {
+      nome: "Maria",
+      sobrenome: "Silva"
+    },
+    {
+      nome: "João",
+      sobrenome: "Pereira"
+    },
+    {
+      nome: "Marcio",
+      sobrenome: "Santos"
+    }
+    ];
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    console.log(this.pessoas);
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10){
+        
+        this.count = 0;
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void{
+    console.log('Clicou em ', nome);
+  }
 }
